7600e22b484250013279a3c476a2f566e531869b
Etienne83 <etienne.gravier83@gmail.com>
2020-04-15 11:05:48 +0200

    4 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,22 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductsAlertsComponent } from './products-alerts/products-alerts.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      {path: 'products/:productId', component: ProductDetailsComponent}
+      {path: 'products/:productId', component: ProductDetailsComponent},
+      {path:'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent},
     ])
   ],
   declarations: [
@@ -24,9 +32,12 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  providers: [CartService]
 })
 export class AppModule { }
 
